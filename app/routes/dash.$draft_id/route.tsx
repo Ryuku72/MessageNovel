4eacb023fb151f2ed033fbc9436bb08762b77586
Ryuku72 <jaybshinsen@hotmail.com>
2024-05-29 23:28:02 +0800

    removed console log another one

--- app/routes/dash.$draft_id/route.tsx
+++ app/routes/dash.$draft_id/route.tsx
@@ -28,8 +28,6 @@ export default function DashNovelId() {
   const navigationState = useNavigation();
   const isLoading = ['submitting', 'loading'].includes(navigationState.state);
 
-
-  console.log(loaderData);
   const LocalStrings = LOCALES.dash.draft;
   const [titleValue, setTitleValue] = useState(loaderData?.title);
 
