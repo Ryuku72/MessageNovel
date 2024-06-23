965a6b38ac4ca1ab702bb8a5fdbd1a3e9e2497b7
Ryuku72 <jaybshinsen@hotmail.com>
2024-06-24 01:09:48 +0800

    style fixes to about page

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -39,16 +39,29 @@ export default function About() {
         <h1 className="text-red-700 text-6xl m-0 font-mono text-center font-miltonian [text-shadow:_5px_3px_2px_rgb(225_225_225_/_50%)]">
           {LocalStrings.title}
         </h1>
-        <div className="flex flex-col gap-10 bg-slate-400 p-6 rounded-xl bg-opacity-25 backdrop-blur-sm shadow-sm">
-          <h2 className="text-4xl m-0 font-mono text-center font-semibold text-gray-800 tracking-wide">
+        <div className="flex flex-col gap-2 bg-gray-100 p-6 rounded-xl bg-opacity-35 backdrop-blur-sm shadow-sm text-gray-800 leading-10">
+          <p className="text-xl leading-10">
+            <strong className="text-3xl pl-4 pr-1">_Message Novel</strong> is a full stack open source project created by <strong className="text-2xl">Joshua K Bader</strong>. 
+          </p>
+
+          <p className="text-xl leading-10">
+          Collaborate with fellow <strong className="text-xl italic">Authors</strong> and friends to create an online novel. <strong className="text-xl italic">Write, comment and promote your stories </strong> with the power of <strong className="text-xl italic">Lexical, Liveblocks and Supabase</strong>.
+          </p>
+          <div className="text-xl">
+            Source Code: <a className="font-semibold text-xl" href="https://github.com/Ryuku72/MessageNovel">https://github.com/Ryuku72/MessageNovel</a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-10 bg-gray-100 p-6 rounded-xl bg-opacity-35 backdrop-blur-sm shadow-sm">
+          <h2 className="text-4xl m-0 font-mono text-center font-semibold text-gray-800 tracking-wide underline">
             Technology Stack
           </h2>
-          <div className="w-full flex flex-wrap justify-center items-center md:gap-16 gap-6">
+          <div className="w-full flex flex-wrap justify-center items-center md:gap-10 gap-6 text-gray-800">
             <ReactLogo svgColor="#00d8ff" uniqueId="credit-React" className="w-auto max-w-full h-28 p-2" />
             <RemixLogo uniqueId="credit-remix" svgColor="none" className="w-auto max-w-full h-20 p-2" />
-            <SupabaseLogo uniqueId="credit-supabase" className="w-auto max-w-full h-28 p-2" />
+            <SupabaseLogo uniqueId="credit-supabase" className="w-auto max-w-full h-24 p-2" />
             <LexicalLogo uniqueId="lexical-logo" className="w-auto max-w-full h-14" />
-            <TailwindLogo uniqueId="credit-twcss" className="w-auto max-w-full h-24 p-2" />
+            <TailwindLogo uniqueId="credit-twcss" className="w-auto max-w-full h-16 p-2" />
             <ViteLogo uniqueId="credit-Vite" className="w-auto max-w-full h-28 p-2" />
             <LiveblocksLogo uniqueId="liveblocks-logo" className="w-auto max-w-full h-16" />
             <BunLogo uniqueId="credit-bun" className="w-auto max-w-full h-28 p-2" />
@@ -57,17 +70,17 @@ export default function About() {
             <VercelLogo uniqueId="credit-vercel" className="w-auto max-w-full h-16 p-2" />
           </div>
         </div>
-        <div className="flex flex-col gap-3 w-full bg-slate-400 p-10 rounded-xl bg-opacity-25 backdrop-blur-sm shadow-sm">
-          <h2 className="text-4xl m-0 font-mono text-center font-semibold text-gray-800 tracking-wide">
+        <div className="flex flex-col gap-3 w-full bg-gray-100 p-10 rounded-xl bg-opacity-35 backdrop-blur-sm shadow-sm">
+          <h2 className="text-4xl m-0 font-mono text-center font-semibold text-gray-800 tracking-wide underline">
             {LocalStrings.background.title}
           </h2>
           <p className="text-2xl text-gray-800 font-semibold">{LocalStrings.background.name}</p>
-          <p className="text-xl text-gray-100">
-            <span className="font-semibold text-gray-800 break-all">Author: </span>
+          <p className="text-xl text-gray-800">
+            <strong className="font-semibold break-all">Author: </strong>
             {LocalStrings.background.author}
           </p>
-          <a href={LocalStrings.background.source} className="text-xl text-gray-100 break-all max-w-full">
-            <span className="font-semibold text-gray-800">Source:</span> {LocalStrings.background.source}
+          <a href={LocalStrings.background.source} className="text-xl text-gray-800 break-all max-w-full">
+            <strong className="font-semibold">Source:</strong> {LocalStrings.background.source}
           </a>
         </div>
         <Link to="/" className="primaryButton py-2.5">
