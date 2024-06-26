3b6e30570b3f7ca6d468d669fc91a65b3bfc50e7
Ryuku72 <jaybshinsen@hotmail.com>
2024-06-26 22:08:36 +0800

    added some console logs to liveblocks

--- app/routes/api.novel/route.ts
+++ app/routes/api.novel/route.ts
@@ -10,10 +10,12 @@ import { initialData } from './initial_state_binary';
 export async function action({ request }: ActionFunctionArgs) {
   if (request.method === 'POST') {
     const data = await request.json();
+    console.dir({ data });
     const id = data.id;
     const { supabaseClient, headers, env } = await initServer(request);
     const userData = await supabaseClient.auth.getUser();
     const user = userData.data?.user;
+    console.dir({ user });
     if (!user?.id) return null;
     const liveblocks = new Liveblocks({ secret: env.LIVEBLOCKS_SECRET_KEY });
 
