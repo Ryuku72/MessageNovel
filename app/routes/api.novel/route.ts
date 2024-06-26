c046a33cab4740d77e6d62ea413fc3372139c4e1
Ryuku72 <jaybshinsen@hotmail.com>
2024-06-26 22:34:36 +0800

    small updates to api.novel

--- app/routes/api.novel/route.ts
+++ app/routes/api.novel/route.ts
@@ -3,32 +3,24 @@ import { ActionFunctionArgs, json } from '@remix-run/node';
 
 import { Liveblocks } from '@liveblocks/node';
 
-import { initServer } from '~/services/API';
+import { envConfig } from '~/services/API';
 
 import { initialData } from './initial_state_binary';
 
 export async function action({ request }: ActionFunctionArgs) {
   if (request.method === 'POST') {
+    const env = envConfig();
     const data = await request.json();
-    console.dir({ data });
-    const id = data.id;
-    const { supabaseClient, headers, env } = await initServer(request);
-    const userData = await supabaseClient.auth.getUser();
-    const user = userData.data?.user;
-    console.dir({ user });
-    if (!user?.id) return null;
-    const liveblocks = new Liveblocks({ secret: env.LIVEBLOCKS_SECRET_KEY });
+    const id = data.record.id;
 
-    const room = await liveblocks.createRoom(id, {
-      defaultAccesses: ['room:write']
-    });
     try {
-    await liveblocks.sendYjsBinaryUpdate(room.id, initialData);
-
-    return json(room, { headers });
+      const liveblocks = new Liveblocks({ secret: env.LIVEBLOCKS_SECRET_KEY });
+      const room = await liveblocks.createRoom(id, {  defaultAccesses: ['room:write'] });
+      await liveblocks.sendYjsBinaryUpdate(room.id, initialData);
+      return json(room);
     } catch (err) {
-        console.error(err);
-        return json(err, { headers, status: 400 });
+      console.error(err);
+      return json(err);
     }
   } else return null;
 }
