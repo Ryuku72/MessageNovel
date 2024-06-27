ad2a4109c3c27cb4cf83dc39802fbcf2604265bd
Ryuku72 <jaybshinsen@hotmail.com>
2024-06-27 10:47:32 +0800

    updates to novel delete end point for pages

--- app/routes/api.novel.delete/route.ts
+++ app/routes/api.novel.delete/route.ts
@@ -10,13 +10,13 @@ export async function action({ request }: ActionFunctionArgs) {
     const data = await request.json();
     const id = data.old_record.id;
 
-    if (!id) throw json('No page id attached', { status: 500 });
+    if (!id) return json('No page id attached', { status: 500 });
     try {
       const liveblocks = new Liveblocks({ secret: env.LIVEBLOCKS_SECRET_KEY });
       await liveblocks.deleteRoom(id);
       return json('success', { status: 200 });
     } catch (err) {
       console.error(err);
-      throw json('Error whilst attempting to delete a room', { status: 500 });
+      return json('Error whilst attempting to delete a room', { status: 500 });
     }
 }
